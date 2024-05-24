26d43d629ed62671c8a0ff0d2bc81105206038e7
unknown <diegoalonsobrmdz@gmail.com>
2024-05-23 19:23:19 -0500

    avanzando

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 /* -------------------------- Import ---------------------- */
 import express from 'express'
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import { engine } from 'express-handlebars';
+
 
 /* ----------------------------- initializacion ----------------------------- */
 const app = express();
+/*evitar colisiones de nombre de variable*/
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /* --------------------------------- setting -------------------------------- */
 app.set('port' , process.env.PORT || 3000 )
+app.set('views' , join(__dirname, 'views'));
+app.engine('.hbs' , engine({
+    defaultLayout: 'main' ,
+    layoutsDir: join(app.get('views'), 'layous'),
+    partialsDir: join(app.get('views'), 'partials'), 
+    extname: '.hbs'
+}));
+app.set('view engine' , '.hbs');
+
 
 /* ------------------------------- middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 /* ---------------------------------- route --------------------------------- */
+app.get('/' , (req, res) => {
+    res.render('index')
+});
 
 /* ------------------------------ public files ------------------------------ */
+app.use(express.static(join(__dirname, 'public')));
 
 /* ------------------------------- run server ------------------------------- */
 app.listen(app.get('port'), () => {
